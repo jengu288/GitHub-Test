36e3cfbf1f88dc5b6080f0110a860f37bd17631a
jengu <jen.jennifer.gu@gmail.com>
2019-09-17 21:12:50 -0700

    Initial Commit

---
+++ GitHub Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitHub Test
+//
+//  Created by Jennifer Gu on 2019-09-17.
+//  Copyright © 2019 Jennifer Gu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
